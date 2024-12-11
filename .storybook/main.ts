fd622ea9dcecf3931557c429f8773b7520731105
dardandauti <33937760+dardandauti@users.noreply.github.com>
2024-12-11 11:01:00 +0100

    Updates

---
+++ .storybook/main.ts
@@ -0,0 +1,28 @@
+import type { StorybookConfig } from "@storybook/react-vite";
+
+const config: StorybookConfig = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    "@storybook/addon-onboarding",
+    "@storybook/addon-essentials",
+    "@chromatic-com/storybook",
+    "@storybook/addon-interactions",
+    "@storybook/addon-a11y",
+    "@storybook/addon-coverage",
+  ],
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+  refs: {
+    "design-system": {
+      title: "My design system",
+      //👇 The url provided by Chromatic when it was deployed
+      url: "https://6752e49f87ce45f40d6912a2-cewkqcgowf.chromatic.com",
+    },
+  },
+};
+export default config;
